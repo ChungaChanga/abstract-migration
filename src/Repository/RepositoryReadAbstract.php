372d85d4e4138e4189e19544390099f346b0c01b
Andrey B <aatuline@gmail.com>
2024-01-04 18:03:26 +0400

    fix repository abstract

--- src/Repository/RepositoryReadAbstract.php
+++ src/Repository/RepositoryReadAbstract.php
@@ -2,6 +2,7 @@
 
 namespace Chungachanga\AbstractMigration\Repository;
 
+use _PHPStan_d5c599c96\Nette\Neon\Exception;
 use Chungachanga\AbstractMigration\Iterator\AwaitingPageIterator;
 
 abstract class RepositoryReadAbstract implements RepositoryReadInterface
@@ -13,5 +14,12 @@ abstract class RepositoryReadAbstract implements RepositoryReadInterface
     ): AwaitingPageIterator
     {
         return new AwaitingPageIterator($this, $startPage, $pageSize, $jumpSize);
+    }
+
+    public function fetchPage(int $page, int $pageSize): array
+    {
+        if ($page < 1) {
+            throw new \InvalidArgumentException('Page must be more than 1');
+        }
     }
 }
